b61a81fd633ee19c80fa17c0657158baffc5c969
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2021-08-20 19:27:33 +0530

    open for new fruits please add few fruits

---
+++ create_fruit.py
@@ -0,0 +1,8 @@
+from fruit import fruits as fu
+
+b1=fu.fruits("banana", "yellow", "sweet")
+print(b1.name)
+print(b1.color)
+print(b1.taste)
+print("___________")
+
---
+++ fruit.py
@@ -0,0 +1,7 @@
+class fruits:
+    def __init__(self, name, color, taste):
+        self.name=name
+        self.color=color
+        self.taste=taste
+
+
