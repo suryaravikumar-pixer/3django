9177a8cd80a912c5607086bd7cb0231095410e78
suryaravikumar-pixer <suryaravikumar5@gmail.com>
2021-08-20 20:45:06 +0530

    conflict solved from locally

--- create_fruit.py
+++ create_fruit.py
@@ -3,6 +3,7 @@ import fruit as fu
 
 b2=fu.fruits("Apple","red","light sweat")
 b1=fu.fruits("banana", "yellow", "sweet")
+b3=fu.fruits("mango","lightorange","yummy_sweet")
 
 print(b2.name)
 print(b2.color)
@@ -13,3 +14,9 @@ print(b1.name)
 print(b1.color)
 print(b1.taste)
 print("___________")
+
+
+
+print(b3.name)
+print(b3.color)
+print(b3.taste)
