bbb548afb31f1892fd535409208c09d3d975d1f9
Durgavasu58 <durgavasu58@gmail.com>
2021-08-20 20:14:37 +0530

    Mango added

--- create_fruit.py
+++ create_fruit.py
@@ -1,13 +1,13 @@
 import fruit as fu
 
 b1=fu.fruits("banana", "yellow", "sweet")
-
+b3=fu.fruits("mango","lightorange","yummy_sweet")
 print(b1.name)
 print(b1.color)
 print(b1.taste)
 print("___________")
 
-b3=fu.fruits("mango","lightorange","yummy_sweet")
+
 
 print(b3.name)
 print(b3.color)
