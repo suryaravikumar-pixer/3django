40bc3b2e9d3dea5139876d993bcb1e470136e188
sridhar <vickysridhar007@gmail.com>
2021-08-20 19:44:39 +0530

    add new object

--- create_fruit.py
+++ create_fruit.py
@@ -1,8 +1,15 @@
-from fruit import fruits as fu
+import fruit as fu
 
+
+b2=fu.fruits("Apple","red","light sweat")
 b1=fu.fruits("banana", "yellow", "sweet")
+
+print(b2.name)
+print(b2.color)
+print(b2.taste)
+print("---------------------------------------")
+
 print(b1.name)
 print(b1.color)
 print(b1.taste)
 print("___________")
-
